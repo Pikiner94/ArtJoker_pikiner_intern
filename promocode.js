ef2b1882614630f910b5d143d3da9f7057f07b8d
Eugene <evgeniykip94@gail.com>
2022-01-20 21:08:19 +0200

    arrowFunction

--- promocode.js
+++ promocode.js
@@ -1,6 +1,66 @@
-document.addEventListener('DOMContentLoaded', askPromoCode());
+const checkOddsSequence = (pairEvenNumbers) => {
+  let result = true;
 
-function askPromoCode() {
+  pairEvenNumbers.forEach((elem) => {
+    if (elem[0] > elem[1]) {
+      result = false;
+    }
+  });
+
+  result ? alert('Your bonus is 2000') : alert('Your bonus is 1000');
+};
+
+const countDigits = (promoCode) => {
+  let splitNumbersPromocode = [];
+
+  while (promoCode) {
+    splitNumbersPromocode.push(promoCode % 10);
+    promoCode = Math.floor(promoCode / 10);
+  }
+
+  splitNumbersPromocode.length != 8
+    ? alert('You entered less than 8 digits')
+    : compareSumEvenOdds(splitNumbersPromocode.reverse());
+};
+
+const checkCountOddsPair = (splitNumbersPromocode) => {
+  let pairEvenNumbers = [];
+
+  splitNumbersPromocode.forEach((elem, index, arr) => {
+    if (
+      !(elem % 2 === 0) &&
+      !(arr[index + 1] % 2 === 0) &&
+      arr[index + 2] % 2 == 0
+    ) {
+      pairEvenNumbers.push(arr.slice(index, index + 2));
+    }
+  });
+
+  pairEvenNumbers.length >= 2
+    ? checkOddsSequence(pairEvenNumbers)
+    : alert('Your bonnus is 100');
+};
+
+const compareSumEvenOdds = (splitNumbersPromocode) => {
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  for (let i = 0; i < splitNumbersPromocode.length; ++i) {
+    if (splitNumbersPromocode[i] % 2) {
+      sumOdd += splitNumbersPromocode[i];
+    } else {
+      sumEven += splitNumbersPromocode[i];
+    }
+  }
+
+  if (sumEven > sumOdd) {
+    checkCountOddsPair(splitNumbersPromocode);
+  } else {
+    alert('0');
+  }
+};
+
+const askPromoCode = () => {
   let isPromoCodeCorrect = false;
   while (!isPromoCodeCorrect) {
     let promoCode = prompt('Enter Promocode');
@@ -15,86 +75,6 @@ function askPromoCode() {
       countDigits(promoCode);
     }
   }
-}
+};
 
-function countDigits(promoCode) {
-  let lengthPromoCode =
-    Math.max(Math.floor(Math.log10(Math.abs(promoCode))), 0) + 1;
-
-  if (lengthPromoCode === 8) {
-    compareSumEvenOdds(promoCode);
-  } else {
-    alert('Promocode is not correct');
-  }
-}
-
-function createSplitNumbers(promoCode) {
-  let splitNumbersPromocode = [];
-
-  while (promoCode) {
-    splitNumbersPromocode.push(promoCode % 10);
-    promoCode = Math.floor(promoCode / 10);
-    break;
-  }
-  return splitNumbersPromocode.reverse();
-}
-
-function compareSumEvenOdds(promoCode) {
-  let sumEven = 0;
-  let sumOdd = 0;
-  const splitNumbersPromocode = createSplitNumbers(promoCode);
-
-  for (let i = 0; i < splitNumbersPromocode.length; ++i) {
-    if (splitNumbersPromocode[i] % 2) {
-      sumOdd += splitNumbersPromocode[i];
-    } else {
-      sumEven += splitNumbersPromocode[i];
-    }
-  }
-
-  if (sumEven > sumOdd) {
-    getResult(promoCode);
-  } else {
-    alert('0');
-  }
-}
-
-function checkEvenOddEntries(promoCode) {
-  let pairNumbers = [];
-
-  while (promoCode) {
-    pairNumbers.push(promoCode % 100);
-    promoCode = Math.floor(promoCode / 100);
-  }
-  pairNumbers.reverse();
-
-  return Boolean(
-    (pairNumbers[0] % 2 && pairNumbers[2] % 2) ||
-      (pairNumbers[1] % 2 && pairNumbers[3] % 2)
-  );
-}
-
-function verifyCequence(promoCode) {
-  const splitNumbersPromocode = createSplitNumbers(promoCode);
-  return Boolean(
-    (splitNumbersPromocode[0] < splitNumbersPromocode[1] &&
-      splitNumbersPromocode[4] < splitNumbersPromocode[5]) ||
-      (splitNumbersPromocode[2] < splitNumbersPromocode[3] &&
-        splitNumbersPromocode[6] < splitNumbersPromocode[7])
-  );
-}
-
-function getResult(promoCode) {
-  const isEvenEntries = checkEvenOddEntries(promoCode);
-  const isNumberCequence = verifyCequence(promoCode);
-
-  if (isEvenEntries && isNumberCequence) {
-    alert('2000');
-  } else if (isEvenEntries && !isNumberCequence) {
-    alert('1000');
-  } else if (!isEvenEntries && !isNumberCequence) {
-    alert('100');
-  } else {
-    alert('0');
-  }
-}
+document.addEventListener('DOMContentLoaded', askPromoCode());
